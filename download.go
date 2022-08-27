efff4bc625baa4e9923580b912362db7aed3416e
lynxcoding <nvrmnd.lynxcoding@manyme.com>
2022-08-27 11:43:10 -0400

    feat: can read comic info from local file or web

--- download.go
+++ download.go
@@ -3,19 +3,58 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 const FirstComic = 1
 const XkcdUrl = "https://xkcd.com/%d/info.0.json"
 const XkcdUrlLatest = "https://xkcd.com/info.0.json"
+const localFile = "xkcd_info.json"
+
+type AllComics struct {
+	Comics []Info
+}
 
 type Info struct {
 	Num              int
 	Title            string
 	Alt              string
 	Year, Month, Day string
+}
+
+func getAll() AllComics {
+	var allComics AllComics
+	jsonFile, err := os.Open(localFile)
+	if err != nil {
+		fmt.Printf("Unable to read local file %s - loading comics from web\n", localFile)
+		allComics = getAllFromWeb()
+		data, err := json.Marshal(allComics)
+		if err != nil {
+			log.Fatalf("JSON marshaling failed: %s", err)
+		}
+		if ioutil.WriteFile(localFile, data, os.ModePerm) != nil {
+			log.Fatalf("Unable to write to file %s: %s", localFile, err)
+		}
+		return allComics
+	}
+	fmt.Printf("Reading comics from file %s\n", localFile)
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &allComics)
+	jsonFile.Close()
+
+	return allComics
+}
+
+func getAllFromWeb() AllComics {
+	var all AllComics
+	for i := FirstComic; i < 5; i++ {
+		all.Comics = append(all.Comics, getNr(i))
+	}
+	return all
 }
 
 func getNr(nr int) Info {
--- search.go
+++ search.go
@@ -10,17 +10,25 @@ import (
 func main() {
 	nr := flag.Int("n", 0, "Get the XKCD comic with this number")
 	random := flag.Bool("r", false, "Get a random XKCD comic")
+	all := flag.Bool("a", false, "Get all XKCD comics")
 	flag.Parse()
 	var comics []Info
-	if *nr > 0 {
+	switch {
+	case *nr > 0:
 		comics = append(comics, getNr(*nr))
-	}
-	if *random {
+	case *random:
 		comics = append(comics, findRandom())
+	case *all:
+		allComics := getAll()
+		for _, v := range allComics.Comics {
+			fmt.Printf("%-5d %-40s %s\n", v.Num, v.Title, v.Alt)
+		}
 	}
-	fmt.Printf("%-5s %-40s %s\n", "Nr", "Title", "Alternative Text")
-	for _, comic := range comics {
-		fmt.Printf("%-5d %-40s %s\n", comic.Num, comic.Title, comic.Alt)
+	if len(comics) > 0 {
+		fmt.Printf("%-5s %-40s %s\n", "Nr", "Title", "Alternative Text")
+		for _, comic := range comics {
+			fmt.Printf("%-5d %-40s %s\n", comic.Num, comic.Title, comic.Alt)
+		}
 	}
 }
 
